ebbfc45011f75bfd23604c1edbaa3c5529327aaa
LeeDongHyun <linear14@naver.com>
2020-12-18 23:09:59 +0900

    Add: LightRGB 데이터 추가

---
+++ app/src/main/java/com/bd/bdproject/data/model/LightRGB.kt
@@ -0,0 +1,7 @@
+package com.bd.bdproject.data.model
+
+data class LightRGB(
+    val r: Int,
+    val g: Int,
+    val b: Int
+) {}
---
+++ app/src/main/java/com/bd/bdproject/util/LightValue.kt
@@ -0,0 +1,27 @@
+package com.bd.bdproject.util
+
+import com.bd.bdproject.data.model.LightRGB
+
+object LightValue {
+    val LEVEL0 = Pair(LightRGB(0, 0, 0), LightRGB(60, 12, 30))
+    val LEVEL1 = Pair(LightRGB(0, 0, 0), LightRGB(80, 24, 41))
+    val LEVEL2 = Pair(LightRGB(0, 0, 0), LightRGB(105, 48, 48))
+    val LEVEL3 = Pair(LightRGB(0, 0, 0), LightRGB(122, 65, 65))
+    val LEVEL4 = Pair(LightRGB(21, 1, 11), LightRGB(135, 87, 76))
+    val LEVEL5 = Pair(LightRGB(21, 1, 11), LightRGB(150, 108, 99))
+    val LEVEL6 = Pair(LightRGB(37, 4, 13), LightRGB(178, 142, 123))
+    val LEVEL7 = Pair(LightRGB(56, 9, 21), LightRGB(200, 164, 143))
+    val LEVEL8 = Pair(LightRGB(78, 24, 24), LightRGB(232, 193, 171))
+    val LEVEL9 = Pair(LightRGB(108, 43, 22), LightRGB(242, 202, 180))
+    val LEVEL10 = Pair(LightRGB(161, 64, 33), LightRGB(238, 201, 180))
+    val LEVEL11 = Pair(LightRGB(201, 69, 27), LightRGB(255, 214, 191))
+    val LEVEL12 = Pair(LightRGB(223, 80, 19), LightRGB(255, 211, 186))
+    val LEVEL13 = Pair(LightRGB(232, 83, 0), LightRGB(255, 228, 195))
+    val LEVEL14 = Pair(LightRGB(255, 107, 0), LightRGB(255, 232, 198))
+    val LEVEL15 = Pair(LightRGB(255, 122, 0), LightRGB(255, 232, 198))
+    val LEVEL16 = Pair(LightRGB(255, 138, 0), LightRGB(255, 232, 198))
+    val LEVEL17 = Pair(LightRGB(255, 160, 17), LightRGB(255, 237, 192))
+    val LEVEL18 = Pair(LightRGB(255, 184, 0), LightRGB(255, 242, 210))
+    val LEVEL19 = Pair(LightRGB(255, 198, 51), LightRGB(255, 243, 212))
+    val LEVEL20 = Pair(LightRGB(255, 205, 77), LightRGB(255, 245, 220))
+}
