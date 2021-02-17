ab6efbc9f7e37b65214c28661cc4a8e2624285a1
LeeDongHyun <linear14@naver.com>
2021-02-17 23:33:34 +0900

    Add: 메모입력 4줄 고정

--- app/src/main/java/com/bd/bdproject/custom_view/MemoEditText.kt
+++ app/src/main/java/com/bd/bdproject/custom_view/MemoEditText.kt
@@ -1,15 +1,19 @@
 package com.bd.bdproject.custom_view
 
+import android.R.attr
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
 import com.bd.bdproject.BitdamLog
+
 
 class MemoEditText: AppCompatEditText {
 
@@ -24,7 +28,13 @@ class MemoEditText: AppCompatEditText {
 
     init {
         setBackgroundResource(android.R.color.transparent)
-        setLineSpacing(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8.0f,  resources.displayMetrics), 1.0f)
+        setLineSpacing(
+            TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                8.0f,
+                resources.displayMetrics
+            ), 1.0f
+        )
         paint.style = Paint.Style.STROKE
         paint.color = ContextCompat.getColor(rootView.context, android.R.color.black)
     }
@@ -52,9 +62,50 @@ class MemoEditText: AppCompatEditText {
         super.onDraw(canvas)
     }
 
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        val watcher: TextWatcher = object : TextWatcher {
+            private var text: String? = null
+            private var beforeCursorPosition = 0
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                text = s.toString()
+                beforeCursorPosition = start
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                /* turning off listener */
+                removeTextChangedListener(this)
+
+                /* handling lines limit exceed */
+                BitdamLog.contentLogger("lineCount: $lineCount, maxLines: $maxLines")
+                if (lineCount > 4) {
+                    setText(text)
+                    setSelection(beforeCursorPosition)
+                }
+
+                /* handling character limit exceed *//*
+                if (s.toString().length > maxCharacters) {
+                    this@LimitedEditText.setText(text)
+                    this@LimitedEditText.setSelection(beforeCursorPosition)
+                    Toast.makeText(context, "text too long", Toast.LENGTH_SHORT)
+                        .show()
+                }*/
+
+                /* turning on listener */
+                addTextChangedListener(this)
+            }
+        }
+        addTextChangedListener(watcher)
+    }
+
     fun setLineColor(color: Int) {
         paint.color = color
     }
-
 
 }
